9b84dc7d415956e6832bc0aedb15e943dc196b91
rudqja1127 <rudqja8297@naver.com>
2023-07-04 13:47:59 +0900

    system exit, article list, 사용자입력 trim() 까지 진행

--- Main_01.java
+++ Main_01.java
@@ -9,13 +9,26 @@ public class Main_01 {
 		System.out.println("== 프로그램 시작 ==");
 		
 		Scanner sc = new Scanner(System.in); 
-		System.out.println("명령어)");
-		String command = sc.nextLine(); //엔터를 치기전 명령어들 모두 출력됨
-		System.out.printf("입력된 명령어 : %s\n", command);
-		String command1 = sc.next();
-		System.out.printf("입력된 명령어 : %s\n", command1);
-		String command2 = sc.next();
-		System.out.printf("입력된 명령어 : %s\n", command2);
+		
+		while(true) {
+			System.out.println("명령어)");
+			String command = sc.nextLine(); //sc.nextLine();엔터를 치기전 명령어들 모두 출력됨
+			command = command.trim(); //앞에 엔터 공백이 있어도 출력이 됨(공백있으면 출력이상해지는데!)
+			
+			if(command.length() == 0){
+				continue; //사용가자 입력을 안하면 계속 명령어) 이거나옴
+			}
+			
+			if(command.equals("system exit")) {
+				break; //영원히 실행됨
+			}
+			else if( command.equals("article list")) {
+				System.out.println("게시물이 없습니다.");
+			}
+			else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
+			}
+		}
 		
 		sc.close();//scanner를 실행할때 마지막에 작성
 		
